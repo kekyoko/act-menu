6aebba4223ccc872ccb595fac810ebfe853c4260
KeKyoko <deman.strus555@gmail.com>
2025-03-17 22:29:28 +0300

    Update act_menu_autorun.lua

--- lua/autorun/act_menu_autorun.lua
+++ lua/autorun/act_menu_autorun.lua
@@ -3,8 +3,5 @@ include("act_menu/sh_config.lua")																																															
 if SERVER then
 	AddCSLuaFile("act_menu/cl_core.lua")
 	AddCSLuaFile("act_menu/sh_config.lua")
-	include("act_menu/sv_core.lua")
-else 
-	include("act_menu/cl_core.lua")
 end
 	
